c112539585740d01c908806f471d70c500f16661
Git sangl003 <sharvari.sangle@gmail.com>
2018-12-13 22:55:00 -0600

    Updates

---
+++ precache-manifest.738756fa4bbaeae6853b785a8d6e2d88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "59843ae1c2ed89599fd0",
+    "url": "/ClickyGame/static/js/runtime~main.59843ae1.js"
+  },
+  {
+    "revision": "aed83dd50202523bdcbf",
+    "url": "/ClickyGame/static/js/main.aed83dd5.chunk.js"
+  },
+  {
+    "revision": "ebe418d19b2b8fbb511f",
+    "url": "/ClickyGame/static/js/1.ebe418d1.chunk.js"
+  },
+  {
+    "revision": "aed83dd50202523bdcbf",
+    "url": "/ClickyGame/static/css/main.d651760c.chunk.css"
+  },
+  {
+    "revision": "ebe418d19b2b8fbb511f",
+    "url": "/ClickyGame/static/css/1.f369de64.chunk.css"
+  },
+  {
+    "revision": "f085d9a6973845fb7d0a6958818793f6",
+    "url": "/ClickyGame/index.html"
+  }
+];
